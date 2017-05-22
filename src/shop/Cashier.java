dc84e52e62c0d72d5888300f58a9205303e57246
Marcury <marcinpiechnik9@wp.pl>
2017-05-22 19:33:56 +0200

    Basic variables, set&get method added.

--- src/shop/Cashier.java
+++ src/shop/Cashier.java
@@ -1,14 +1,17 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package shop;
 
 /**
  *
- * @author Admin
+ * @author Marcury
  */
 public class Cashier {
+    private int id;
     
+    public int getId(){
+        return id;
+    }
+    
+    public void setId(int id){
+        this.id= id;
+    }
 }
