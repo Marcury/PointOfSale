429bd38b785fe37da1e765e0b4b29227a7e73c8b
Marcury <marcinpiechnik9@wp.pl>
2017-05-22 19:39:27 +0200

    Status method added. It shows current status of object instance.

--- src/shop/Product.java
+++ src/shop/Product.java
@@ -1,14 +1,59 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package shop;
 
 /**
  *
- * @author Admin
+ * @author Marcury
  */
+
 public class Product {
+    private String name;
+    private Price price;
+    private int barcode;
+    private String description;
+    private int amount;                 //number of products in stock
+
+    public String status(){        // prints status of this Object instance
+        return name + "," + barcode + "," + price + "," + amount +
+                "," + price.getAmount() + "," + price.getCurrency();
+    }
     
+    
+    public void setPrice(Price price){
+        this.price = price;
+    }
+    public Price getPrice(){
+        return price;
+    }
+    
+    public void setBarcode(int barcode){
+        this.barcode = barcode;
+    }
+    
+    public int getBarcode(){
+        return barcode;
+    }
+    
+    public void setDescription(String descryption){
+        this.description = descryption;
+    }
+    
+    public String getDescription() {
+        return description;
+    }
+    
+    public void setAmount(int amount){
+        this.amount = amount;
+    }
+    
+    public int getAmount(){
+        return amount;
+    }
+    
+    public void setName(String name){
+        this.name = name;
+    }
+    
+    public String getName(){
+        return name;
+    }
 }
